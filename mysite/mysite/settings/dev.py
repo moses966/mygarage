504a4a6eeadada9b32f4efe08cae40c1e1578f37
Muwawu Moses <mosesmuwawu@gmail.com>
2023-10-09 22:58:48 +0300

    First commit

---
+++ mysite/mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-1d46y2+h-+e*b#aa-6!_s2$qn-*4(#kv+5u@rzv8lpmx2kn7c-"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
