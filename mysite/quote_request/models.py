6be7c2d13cf733b1d41a5d1b9c115172af7809cc
Muwawu Moses <mosesmuwawu@gmail.com>
2023-10-28 09:14:46 +0300

    Added a functional Form

--- mysite/quote_request/models.py
+++ mysite/quote_request/models.py
@@ -1,9 +1,37 @@
 from django.db import models
+from modelcluster.fields import ParentalKey
+from wagtail.admin.panels import (
+    FieldPanel, FieldRowPanel,
+    InlinePanel, MultiFieldPanel
+)
+from wagtail.fields import RichTextField
+from wagtail.contrib.forms.models import AbstractEmailForm, AbstractFormField
 
-# model to store quote requests
-class QuoteRequest(models.Model):
-    name = models.CharField(max_length=100)
-    email = models.EmailField()
-    phone = models.CharField(max_length=20)
-    industry = models.CharField(max_length=100)
-    message = models.TextField()
+# Define a custom form field that is associated with a FormPage
+class FormField(AbstractFormField):
+    page = ParentalKey('FormPage', on_delete=models.CASCADE, related_name='custom_form_fields')
+
+# Define a FormPage model, which inherits from AbstractEmailForm
+class FormPage(AbstractEmailForm):
+    # Introduction text for the form page
+    intro = RichTextField(blank=True)
+    # Thank you text to display after successful form submission
+    thank_you_text = RichTextField(blank=True)
+
+    # Define content panels for the FormPage
+    content_panels = AbstractEmailForm.content_panels + [
+        FieldPanel('intro'),
+        InlinePanel('custom_form_fields', label="Form fields"),   # Inline panel for custom form fields
+        FieldPanel('thank_you_text'),
+        MultiFieldPanel([
+            FieldRowPanel([
+                FieldPanel('from_address', classname="col6"),    # Email sender address
+                FieldPanel('to_address', classname="col6"),  # Email recipient address
+            ]),
+            FieldPanel('subject'),   # Email subject
+        ], "Email"),   # Panel for email configuration
+    ]
+    
+     # Method to get all form fields associated with this FormPage
+    def get_form_fields(self):
+        return self.custom_form_fields.all()
--- mysite/quote_request/views.py
+++ mysite/quote_request/views.py
@@ -1,8 +1,1 @@
-from django.shortcuts import render
-from .models import QuoteRequest  # Importing the QuoteRequest model
 
-def quote_request_list(request):
-    # Retrieve all quote requests from the database
-    quote_requests = QuoteRequest.objects.all()
-
-    return render(request, 'quote_request/quote_request_list.html', {'quote_requests': quote_requests})
