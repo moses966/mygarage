6a4acc94198c1fe70f595d866bbe4d3bcf9518c0
Muwawu Moses <mosesmuwawu@gmail.com>
2023-11-23 11:56:56 +0300

    added; about-us page

---
+++ mysite/about/blocks.py
@@ -0,0 +1,24 @@
+
+from wagtail.blocks import StructBlock
+from wagtail import blocks
+
+
+class AboutBlock(StructBlock):
+    heading = blocks.CharBlock(max_length='45')
+    intro = blocks.CharBlock(max_length='250')
+    mini_para1 = blocks.CharBlock(max_length='120')
+    mini_para2 = blocks.CharBlock(max_length='120')
+    outro = blocks.CharBlock(max_length='250')
+
+class GoalBlock(StructBlock):
+    header = blocks.RichTextBlock()
+    text = blocks.RichTextBlock()
+
+class Director(StructBlock):
+    heading1 = blocks.RichTextBlock()
+    message = blocks.RichTextBlock()
+
+class Faqs(StructBlock):
+    question = blocks.CharBlock(max_length='50')
+    answer = blocks.CharBlock(max_length='250')
+
---
+++ mysite/about/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+from wagtail.models import Page
+from wagtail.admin.panels import FieldPanel, InlinePanel
+from wagtail.fields import StreamField
+from .blocks import AboutBlock, Director, Faqs, GoalBlock
+
+
+class AboutPage(Page):
+    body = StreamField([
+        ('about_block', AboutBlock()),
+        ('about_director', Director()),
+        ('faqs', Faqs()),
+        ('goals', GoalBlock()),
+    ], use_json_field=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('body'),
+    ]
